018492db7f9453ebc1a47b2157ba4efa729c3e70
KarlPreisler <Karlpreisler@gmail.com>
2023-05-23 15:00:37 +0000

    Simplified Timer function

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,24 @@
-"use strict";
+document.addEventListener('DOMContentLoaded', function() {
+  let timer = null;
+  let seconds = 30;
 
-let hour = 0;
-let minute = 0;
-let second = 0;
-let millisecond = 0;
-
-let cron;
-
-document.form_main.start.onclick = () => start();
-document.form_main.pause.onclick = () => pause();
-document.form_main.reset.onclick = () => reset();
-
-function start() {
-    pause();
-    cron = setInterval(() => { timer(); }, 10);
-}
-
-function pause() {
-    clearInterval(cron);
+  function startTimer() {
+    timer = setInterval(function() {
+      seconds--;
+      if (seconds < 0) {
+        clearInterval(timer);
+        alert('Time is up');
+      } else {
+        console.log(seconds);
+      }
+    }, 1000);
   }
 
-function reset() {
-    second = 0;
-    millisecond = 0;
-
-    document.getElementById('second').innerText = '00';
+  function stopTimer() {
+    clearInterval(timer);
+    seconds = 30;
   }
 
-  function timer() {
-    if ((millisecond += 10) == 1000) {
-      millisecond = 0;
-      second++;
-    }
-    document.getElementById('second').innerText = returnData(second);
-    document.getElementById('millisecond').innerText = returnData(millisecond);
-  }
-  
-  function returnData(input) {
-    return input > 10 ? input : `0${input}`
-  }
+  document.getElementById('start-btn').addEventListener('click', startTimer);
+  document.getElementById('stop-btn').addEventListener('click', stopTimer);
+});
